fd0cf55854ce0d3fca9703f17d2dd14a9a34cc79
mihec <mihael.vercek@gmail.com>
2019-10-05 21:00:28 +0200

    Added WebView

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import {ListComponent} from "./home/transports/list/list.component";
+import {MapComponent} from "./map/map.component";
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'list-bicycle', component: ListComponent },
+    { path: 'home/map', component: MapComponent}
 ];
 
 @NgModule({
--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './home/transports/list/list.component';
+import { MapComponent } from './map/map.component';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from 'nativescript-angular/forms';
@@ -17,6 +18,7 @@ import { ListComponent } from './home/transports/list/list.component';
       AppComponent,
       HomeComponent,
       ListComponent,
+      MapComponent,
   ],
   imports: [
       NativeScriptModule,
