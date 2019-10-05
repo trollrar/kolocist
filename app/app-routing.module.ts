dea6cabbebd927afbfee628e3c00e0e96d6ebea4
mihec <mihael.vercek@gmail.com>
2019-10-06 00:41:55 +0200

    Added Add Item to side bar

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import {MapComponent} from "./main/map/map.component";
 import {ProfileComponent} from "./main/profile/profile.component";
+import {AddItemComponent} from "./main/add-item/add-item.component";
 
 const routes: Routes = [
     { path: '', redirectTo: '/main', pathMatch: 'full' },
     { path: 'main', component: MainComponent, children: [
             { path: 'map', component: MapComponent},
-            { path: 'profile', component: ProfileComponent}
+            { path: 'profile', component: ProfileComponent},
+            { path: 'add-item', component: AddItemComponent }
         ] },
 
 ];
