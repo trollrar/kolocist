a16f026f452e65349c92c27ff41e6c3c1404d184
farescalms <fares@calms.eu>
2019-10-05 21:22:33 +0200

    fixed project structure

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import {ListComponent} from "./home/transports/list/list.component";
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'list-bicycle', component: ListComponent },
+    { path: '', redirectTo: '/main', pathMatch: 'full' },
+    { path: 'main', component: MainComponent }
 ];
 
 @NgModule({
--- app/main/main.module.ts
+++ app/main/main.module.ts
@@ -1,9 +1,8 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
-import { AppRoutingModule } from './app-routing.module';
+import { MainRoutingModule } from './main-routing.module';
 
-import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
+import { MainComponent } from './main.component';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from 'nativescript-angular/forms';
@@ -12,16 +11,14 @@ import { MainComponent } from './main/main.component';
 // import { NativeScriptHttpClientModule } from 'nativescript-angular/http-client';
 
 @NgModule({
-  declarations: [
-      AppComponent,
-      MainComponent,
-  ],
-  imports: [
-      NativeScriptModule,
-      AppRoutingModule,
-  ],
-  bootstrap: [AppComponent],
-  schemas: [NO_ERRORS_SCHEMA],
+    declarations: [
+        MainComponent,
+    ],
+    imports: [
+        NativeScriptModule,
+        MainRoutingModule,
+    ],
+    bootstrap: [MainComponent],
+    schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule {}
-
